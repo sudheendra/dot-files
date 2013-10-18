01b99c13754d09b220e3333a40e6678e9d7cc3b9
Arun Chandrasekaran <visionofarun@gmail.com>
2013-10-18 15:05:41 +0530

    Fixed a typo in the installer

--- .vim/.vim
+++
@@ -1,1 +0,0 @@
-/home/arun/env-setup/.vim
--- install.sh
+++ install.sh
@@ -15,11 +15,13 @@ ln -sf $PWD/.valgrindrc ~/.valgrindrc
 echo "Created links. Now you can change the configuration files as required or use the defaults."
 echo "Notable files to be changed: .gitconfig .hgrc .vimrc .alias .bashrc"
 echo -n "Modify the configuration files? (y/n)"
-read -n ans
+read -n1 ans
 if [ "$ans" = "y" -o "$ans" = "Y" ]; then
     vim .hgrc
     vim .gitconfig
     vim .vimrc
     vim .alias
     vim .bashrc
+else
+    echo
 fi
